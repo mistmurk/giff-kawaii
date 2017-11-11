59012530fd0e53d8c7ef96e26b34ee610d5f6918
Richard Yu <richardyu042@gmail.com>
2017-11-11 16:21:20 -0500

    updating package and index for api

--- index.js
+++ index.js
@@ -1,5 +1,10 @@
 var Alexa = require("alexa-sdk");
 
+const Mal = require('../main.js');
+const auth = require('./auth.json');
+
+let mal = new Mal(auth);
+
 exports.handler = function(event, context, callback){
     var alexa = Alexa.handler(event, context, callback);
 };
@@ -11,3 +16,18 @@ var handlers = {
     }
 
 };
+
+
+mal.verifyCredentials()
+	.then(user => console.log(user.username + 'verified'))
+	.catch(err => console.error(err));
+
+animeSearch = function(inputString){
+	mal.searchEntry('anime', inputString)
+		.then(animes => {
+			for(let anime of animes){
+				console.log(anime.title);
+			}
+		})
+		.catch(err => console.error(error));
+}
