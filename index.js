7443041c01e3954b1ad2efac024a5ff091cfd33e
Luke Vacek <lukev21@gmail.com>
2017-11-11 22:49:00 -0500

    changed to read description

--- index.js
+++ index.js
@@ -9,9 +9,6 @@ const mal = new Mal({
 	password,
 })
 
-mal.account.verifyCredentials()
-	.then(res => console.log(res))
-	.catch(err => console.log("wtf"));
 
 exports.handler = function(event, context, callback){
     let alexa = Alexa.handler(event, context);
@@ -21,21 +18,6 @@ exports.handler = function(event, context, callback){
 
 const handlers = {
 
-    'SearchIntent': function () {
-      var alexaString = this.event.request.intent.slots.anime.value;
-
-      var objectJ = lookUpAnime(alexaString, () => {
-      	var retString = ""
-      	if (objectJ == null) {
-        	retString = "I can't find ".concat(alexaString);
-      	} else {
-        	retString = objectJ.title;
-      	}
-
-      	this.emit(':tell', retString);
-      });
-
-    },
     'AMAZON.CancelIntent': function () {
         this.emit(':tell', "Sayonara!");
     },
@@ -44,6 +26,23 @@ const handlers = {
     },
     'AMAZON.StopIntent': function() {
         this.emit(':tell', "Sayonara!");
+    },'SearchIntent': function () {
+      var alexaString = this.event.request.intent.slots.anime.value;
+
+      var objectJ = lookUpAnime(alexaString, (result) => {
+				if(result instanceof Array) {
+					result = result[0];
+				}
+      	var retString = ""
+      	if (result == null) {
+        	retString = "I can't find ".concat(alexaString);
+      	} else {
+        	retString = decodeURI(unescape(result.synopsis));
+      	}
+
+      	this.emit(':tell', retString);
+      });
+
     }
 
 };
